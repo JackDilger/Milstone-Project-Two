bbca386b53c3f20767221046cec3eaa71e04b10b
JackDilger <jack.dilger1995@gmail.com>
2022-02-17 17:59:50 +0000

    Add question and answer area to quiz.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,24 @@
+
+/*---------------------------------------------------- declared variables*/
 const instructions = document.getElementById('instruction-btn');
 const modalContainer = document.getElementById('modal-container');
 const closeModal = document.getElementById('close-modal-btn');
+const start = document.getElementById('start-btn');
 
+
+/*---------------------------------------------------- show modal */
 instructions.addEventListener('click', () => {
     modalContainer.classList.add('show');
 });
 
+
+/*---------------------------------------------------- hide modal */
 closeModal.addEventListener('click', () => {
     modalContainer.classList.remove('show');
 });
 
-
-
-const start = document.getElementById('start-btn');
-
+/*---------------------------------------------------- open quiz */
 start.onclick = function() {
     location.href = "quiz.html";
 };
+
