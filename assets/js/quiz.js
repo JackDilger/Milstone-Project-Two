ad3f9a58ac81e9a28f25ab0c9a6dc45236b39021
JackDilger <jack.dilger1995@gmail.com>
2022-02-18 16:43:40 +0000

    Create arrow function to shuffle through questions array

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,21 +1,23 @@
 /*---------------------------------------------------- declared variables*/
+let shuffledQuestions, currentQuestionIndex
 
 
 
 /*---------------------------------------------------- Start Quiz*/
 
-function startQuiz () {
-
+function startQuiz() {
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
 }
 
 
 /*----------------------------------------------------  Selects a question*/
-function setQuestion () {
+function setQuestion() {
 
 }
 
 
 /*---------------------------------------------------- Checks if the answer is true or false*/
-function answer () {
+function answer() {
 
 }
