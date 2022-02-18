b514c5ec547b417dd13b622cfcecc8cd25ac43a7
JackDilger <jack.dilger1995@gmail.com>
2022-02-18 17:32:23 +0000

    Move quiz questions array in to quiz.js and fix script link naming

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,21 +1,335 @@
+const questions = [{
+    question: "What color are Raichu's cheeks?",
+    answers: [{
+            text: "Yellow",
+            correct: true,
+        },
+        {
+            text: "Red",
+            correct: false,
+        },
+        {
+            text: "Blue",
+            correct: false,
+        },
+        {
+            text: "Orange",
+            correct: false,
+        },
+    ],
+},
+{
+    question: "What year was the first pokemon game released",
+    answers: [{
+            text: "1996",
+            correct: true,
+        },
+        {
+            text: "1995",
+            correct: false,
+        },
+        {
+            text: "1991",
+            correct: false,
+        },
+        {
+            text: "1992",
+            correct: false,
+        },
+    ],
+},
+{
+    question: "How many types of Pokemon are there?",
+    answers: [{
+            text: "21",
+            correct: false,
+        },
+        {
+            text: "24",
+            correct: false,
+        },
+        {
+            text: "18",
+            correct: true,
+        },
+        {
+            text: "16",
+            correct: false,
+        },
+    ],
+},
+{
+    question: "How many Gym Badges must a trainer collect before challenging the Elite Four?",
+    answers: [{
+            text: "9",
+            correct: false,
+        },
+        {
+            text: "6",
+            correct: false,
+        },
+        {
+            text: "7",
+            correct: false,
+        },
+        {
+            text: "8",
+            correct: true,
+        },
+    ],
+},
+
+{
+    question: "What is HM07?",
+    answers: [{
+            text: "Rock Smash",
+            correct: false,
+        },
+        {
+            text: "Waterfall",
+            correct: true,
+        },
+        {
+            text: "Flash",
+            correct: false,
+        },
+        {
+            text: "Dive",
+            correct: false,
+        },
+    ],
+},
+{
+    question: "What planet is known as the red planet?",
+    answers: [{
+            text: "Mars",
+            correct: true,
+        },
+        {
+            text: "Venus",
+            correct: false,
+        },
+        {
+            text: "Uranus",
+            correct: false,
+        },
+        {
+            text: "Neptune",
+            correct: false,
+        },
+    ],
+},
+{
+    question: "What Pokemon can be resurrected from the Old Amber?",
+    answers: [{
+            text: "Aerodactly",
+            correct: true,
+        },
+        {
+            text: "Kabuto",
+            correct: false,
+        },
+        {
+            text: "Anorith",
+            correct: false,
+        },
+        {
+            text: "Lileep",
+            correct: false,
+        },
+    ],
+},
+{
+    question: "Who was the first person to walk on the moon?",
+    answers: [{
+            text: "Neil Armstrong ",
+            correct: true,
+        },
+        {
+            text: "Elon Musk",
+            correct: false,
+        },
+        {
+            text: "William Shatner",
+            correct: false,
+        },
+        {
+            text: "Tim Peake",
+            correct: false,
+        },
+    ],
+},
+{
+    question: "What type of Pokemon does Gym Leader Erika specialize in?",
+    answers: [{
+            text: "Water",
+            correct: false,
+        },
+        {
+            text: "Psychic",
+            correct: false,
+        },
+        {
+            text: "Grass",
+            correct: true,
+        },
+        {
+            text: "Electric",
+            correct: false,
+        },
+    ],
+},
+{
+    question: "What types is Charizard listed as?",
+    answers: [{
+            text: "Fire & Dragon",
+            correct: false,
+        },
+        {
+            text: "Fire & Flying",
+            correct: true,
+        },
+        {
+            text: "Flying & Dragon",
+            correct: false,
+        },
+        {
+            text: "Fire",
+            correct: false,
+        },
+    ],
+},
+{
+    question: "Which of these Pokemon is the tallest??",
+    answers: [{
+            text: "Charizard",
+            correct: false,
+        },
+        {
+            text: "Venusaur",
+            correct: true,
+        },
+        {
+            text: "Blastoise",
+            correct: false,
+        },
+        {
+            text: "Rhydon",
+            correct: false,
+        },
+    ],
+},
+{
+    question: "Who is #100 on the Pokedex?",
+    answers: [{
+            text: "Mewtwo",
+            correct: false,
+        },
+        {
+            text: "Voltorb",
+            correct: true,
+        },
+        {
+            text: "Haunter",
+            correct: false,
+        },
+        {
+            text: "Kangaskhan",
+            correct: false,
+        },
+    ],
+},
+{
+    question: "What is the best selling Pokemon game ever?",
+    answers: [{
+            text: "Pokemon Red & Blue",
+            correct: true,
+        },
+        {
+            text: "Pokemon Gold & Silver",
+            correct: false,
+        },
+        {
+            text: "Pokemon Leaf Green & Fire Red",
+            correct: false,
+        },
+        {
+            text: "Pokemon X & Y",
+            correct: false,
+        },
+    ],
+},
+{
+    question: "Which is the most common type of Pokemon?",
+    answers: [{
+            text: "Ground",
+            correct: false,
+        },
+        {
+            text: "Water",
+            correct: true,
+        },
+        {
+            text: "Grass",
+            correct: false,
+        },
+        {
+            text: "Normal",
+            correct: false,
+        },
+    ],
+},
+{
+    question: "What is the name of the Pokemon which was Ash Ketchum’s first catch?",
+    answers: [{
+            text: "Pikachu",
+            correct: false,
+        },
+        {
+            text: "Bulbasur",
+            correct: false,
+        },
+        {
+            text: "Snorlax",
+            correct: false,
+        },
+        {
+            text: "Caterpie",
+            correct: true,
+        },
+    ],
+},
+];
+
+
+
 /*---------------------------------------------------- declared variables*/
-let shuffledQuestions, currentQuestionIndex
+let shuffledQuestions;
+let currentQuestionIndex;
+const questionText = document.getElementById('question-text')
 
 
+startQuiz();
 
 /*---------------------------------------------------- Start Quiz*/
 
 function startQuiz() {
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
-}
+} 
 
 
 /*----------------------------------------------------  Selects a question*/
-function setQuestion() {
+function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 
 }
 
+/*----------------------------------------------------  Selects a question*/
+function showQuestion(question) {
+    questionText.innerText = question.question
+
+}
 
 /*---------------------------------------------------- Checks if the answer is true or false*/
 function answer() {
