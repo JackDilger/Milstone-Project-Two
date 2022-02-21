9ad81143640c31fae003d5a0bfe3f7509661e5ed
JackDilger <jack.dilger1995@gmail.com>
2022-02-21 14:39:00 +0000

    Make next button increment through questions once answer is selected

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -5,6 +5,10 @@ const answerButtons = document.getElementById('answer-buttons')
 
 let shuffledQuestions, currentQuestionIndex
 
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
 
 startQuiz();
 
