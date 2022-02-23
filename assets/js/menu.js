db15470d0e36ee09e0abcd2aeb275f9b1c113a7d
JackDilger <jack.dilger1995@gmail.com>
2022-02-23 18:26:48 +0000

    Add style to prize modal

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -8,21 +8,35 @@ const instructions = document.getElementById('instruction-btn');
 const modalContainer = document.getElementById('modal-container');
 const closeModal = document.getElementById('close-modal-btn');
 const start = document.getElementById('start-btn');
+const prize = document.getElementById('prize-btn')
+const prizemodalContainer = document.getElementById('prize-modal-container');
+const closePrizeModal = document.getElementById('close-prize-modal-btn');
 
-
-// Show modal 
-instructions.addEventListener('click', () => {
-    modalContainer.classList.add('show');
-});
-
-
-// Hide modal 
-closeModal.addEventListener('click', () => {
-    modalContainer.classList.remove('show');
-});
 
 // Open quiz question area
 start.onclick = function() {
     location.href = "quiz.html";
 };
 
+// Show instruction modal 
+instructions.addEventListener('click', () => {
+    modalContainer.classList.add('show');
+});
+
+
+// Hide instruction modal 
+closeModal.addEventListener('click', () => {
+    modalContainer.classList.remove('show');
+});
+
+
+// Show prize modal 
+prize.addEventListener('click', () => {
+    prizemodalContainer.classList.add('show');
+});
+
+
+// Hide prize modal 
+closePrizeModal.addEventListener('click', () => {
+    prizemodalContainer.classList.remove('show');
+});
