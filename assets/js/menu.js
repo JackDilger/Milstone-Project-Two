e57bc7cd7f91838d09417ae604b04e66e04853a1
JackDilger <jack.dilger1995@gmail.com>
2022-02-23 17:51:05 +0000

    Add prize draw button and ammend style of menu controls

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,23 +1,27 @@
+/**
+ * Modal credit Florin Pop tutorial video, ammended code for my quiz
+ * see REAMDME.md credit section
+ */
 
-/*---------------------------------------------------- declared variables*/
+// Declared variables
 const instructions = document.getElementById('instruction-btn');
 const modalContainer = document.getElementById('modal-container');
 const closeModal = document.getElementById('close-modal-btn');
 const start = document.getElementById('start-btn');
 
 
-/*---------------------------------------------------- show modal */
+// Show modal 
 instructions.addEventListener('click', () => {
     modalContainer.classList.add('show');
 });
 
 
-/*---------------------------------------------------- hide modal */
+// Hide modal 
 closeModal.addEventListener('click', () => {
     modalContainer.classList.remove('show');
 });
 
-/*---------------------------------------------------- open quiz question area*/
+// Open quiz question area
 start.onclick = function() {
     location.href = "quiz.html";
 };
